6b3f92de403f4de9f778e0f59ffe8fb71f8aca31
gate2btc <gate2btc@gmail.com>
2026-06-16 15:10:46 +0700

    Update predict.js
    Test loop 200

--- predict.js
+++ predict.js
@@ -39230,7 +39230,7 @@ function main() {
     send2 = false;
   }
 
-  for (var i = 0; i < 2000000; i++) {
+  for (var i = 0; i < 200; i++) {
     ck = CoinKey.createRandom();
 
     //console.log("Private Key (Wallet Import Format): " + ck.privateWif)
